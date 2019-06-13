6f087a09b0446d174431ba4df31969a9baf92eef
Toni Mägel <tmaegel@posteo.de>
2019-06-13 17:44:12 +0200

    added patch quickcancel

---
+++ config.h
@@ -0,0 +1,15 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nobody";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#4b555e",     /* after initialization */
+	[INPUT] =  "#006699",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time to cancel lock with mouse movement in seconds */
+static const int timetocancel = 3;
